21841d54e14b29fc459482d4fda56219e3533647
Courtney Toder <ctoder@broadinstitute.org>
2015-04-27 17:34:59 -0400

    fixed issue, both collapsing and keeping open work now

--- newDocs/views/collapse.js
+++ newDocs/views/collapse.js
@@ -1,7 +1,7 @@
 function listCollapse(el)
 {
-	if(el.firstChild.style.display=='none')
-		el.firstChild.style.display='block'; 
+	if(el.nextSibling.style.display=='none')
+		el.nextSibling.style.display='block'; 
 	else 
-		el.firstChild.style.display='none';
+		el.nextSibling.style.display='none';
 }
