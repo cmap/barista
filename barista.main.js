945bc2a564d8e243748cd7579dee99ed9d8fc349
Corey Flynn <flynn.corey@gmail.com>
2015-06-08 12:21:51 -0400

    prefilter switch to headers

--- barista.main.js
+++ barista.main.js
@@ -4454,12 +4454,16 @@ Barista.numberWithCommas = function(x){
 Barista.setAPIPrefilter = function(api_endpoint) {
     api_endpoint = (api_endpoint !== undefined) ? api_endpoint : 'api.lincscloud.org';
     // configure ajax calls to add the user key parameter on calls to api.lincscloud.org
-    $.ajaxPrefilter(function( options, originalOptions, jqXHR ){
+    // $.ajaxPrefilter(function ( options, originalOptions, jqXHR ){
+    $.ajaxPrefilter(function ( options){
         var re = new RegExp(api_endpoint);
         var re_default = new RegExp('api.lincscloud.org');
-        if (re.test(options.url) || re_default.test(options.url)){
-            options.data = $.param($.extend(originalOptions.data,{user_key:Barista.user_key}));
+        options.beforeSend = function (xhr) {
+            xhr.setRequestHeader('user_key', Barista.user_key);
         }
+        // if (re.test(options.url) || re_default.test(options.url)){
+        //     options.data = $.param($.extend(originalOptions.data,{user_key:Barista.user_key}));
+        // }
     });
 };
 
